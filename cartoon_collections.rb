6bcdb2cae4982babadc5b905368b44507ded87a1
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 16:47:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? do
+  calls.any? do | call |
+    call.length
 end
 
 def find_the_cheese# code an argument here
