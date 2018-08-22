f89e63cfa2443a585c3ea2de72d0679c91263c88
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 16:58:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(strings)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find do | cheese |
     cheese.cheese_types?
   end
